6d4e4451249dc4ffed009e0241b76a68b40d0ece
Andrey <andrey291188@gmail.com>
2023-09-23 08:58:38 +0300

    up

--- middlewares/authenticate.js
+++ middlewares/authenticate.js
@@ -7,14 +7,13 @@ const {SECRET_KEY} = process.env
 const authenticate = async (req, res, next) => {
     const {authorization = ""} = req.headers;
     const [bearer, token] = authorization.split(" ");
-    if (bearer !== "Bearer") {
+    if (bearer !== "Bearer" || token === "") {
         next(HttpError(401, "Not authorized"))
     }
     try {
         const {id} = jwt.verify(token, SECRET_KEY);
-        console.log(id)
         const user = await User.findById(id);
-        console.log(user)
+      
         if(!user || !user.token || user.token !== token) {
             next(HttpError(401, "Not authorized"))
         }
